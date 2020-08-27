5536ab88b0260da0ee4475ee15927a9861f17c1a
pauher <pauline.hering@sinnerschrader.com>
2020-08-27 16:05:12 +0200

    feat: add styling to detail page

--- src/pages/detail-page.tsx
+++ src/pages/detail-page.tsx
@@ -6,6 +6,28 @@ import ErrorPage from "./error-detail-page";
 import { useHistory } from "react-router-dom";
 import StarRating from "../components/star-rating";
 
+const HeroGrid = styled.div`
+  display: grid;
+  grid-template-columns: 2fr 3fr;
+`;
+
+const HeroText = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+const Description = styled.p`
+  max-width: 650px;
+  margin: 0 auto;
+`;
+
+const Review = styled.div`
+  max-width: 650px;
+  margin: 0 auto;
+`;
+
 const BackButton = styled.button`
   display: inline-block;
   border-radius: 3px;
@@ -32,25 +54,34 @@ function DetailPage() {
   return (
     <>
       <BackButton onClick={navigateToOverview}>Back To Overview</BackButton>
-      <img alt="" src={wine?.imagePath} height="200px" />
-      <h1>{wine.name}</h1>
-      <p>{wine.year}</p>
-      <p>{wine.type}</p>
-      <p>{wine.specialties}</p>
-      <p>Vol: {wine.vol}%</p>
-      <br />
-      <p>{wine.description}</p>
-      <h3>Price per bottle: {wine?.price}</h3>
-      <br />
-      {wine.reviews?.map((review) => {
-        return (
+      <HeroGrid>
+        <img alt="" src={wine?.imagePath} height="400px" />
+        <HeroText>
           <div>
-            <h4>{review.name}</h4>
-            <p>{review.text}</p>
-            <StarRating rating={review.rating} />
+            <h1>{wine.name}</h1>
+            <p>{wine.year}</p>
+            <p>{wine.type}</p>
+            <p>{wine.specialties}</p>
+            <p>Vol: {wine.vol}%</p>
+            <h3>Price per bottle: {wine?.price}</h3>
           </div>
-        );
-      })}
+        </HeroText>
+      </HeroGrid>
+      <Description>
+        <p>{wine.description}</p>
+      </Description>
+      <Review>
+        {wine.reviews?.map((review) => {
+          return (
+            <div>
+              <h4>{review.name}</h4>
+              <p>{review.text}</p>
+              <StarRating rating={review.rating} />
+              <p>{review.rating}</p>
+            </div>
+          );
+        })}
+      </Review>
     </>
   );
 }
