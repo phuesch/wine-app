49694da38b5fe9bd35014da5b5665a4daf04762b
pauher <pauline.hering@sinnerschrader.com>
2020-09-25 13:38:02 +0200

    fix: adjust styling of loading spinner

--- src/components/loadingSpinner.tsx
+++ src/components/loadingSpinner.tsx
@@ -1,11 +1,19 @@
-import BarLoader from "react-spinners/ClipLoader";
+import BarLoader from "react-spinners/BarLoader";
 import React from "react";
+import { css } from "@emotion/core";
+import styled from "styled-components";
+
+const StyledLoader = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: 400px;
+`;
 
 const LoadingSpinner = () => {
   return (
-    <div>
-      <BarLoader size={150} color={"red"} loading={true} />
-    </div>
+    <StyledLoader>
+      <BarLoader color={"black"} loading={true} width={"400px"} />
+    </StyledLoader>
   );
 };
 
