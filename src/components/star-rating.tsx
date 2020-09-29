a83bdff458f388c25d744d033a9d7a181ab4feb2
pauher <pauline.hering@sinnerschrader.com>
2020-09-29 13:51:31 +0200

    fix: styled star rating

--- src/components/star-rating.tsx
+++ src/components/star-rating.tsx
@@ -1,5 +1,5 @@
 import React, { FC, ReactNode } from "react";
-import { Star } from "react-feather";
+import { StarRegular, StarSolid } from "./stars";
 
 interface StarRatingProps {
   rating: number;
@@ -7,8 +7,12 @@ interface StarRatingProps {
 
 const StarRating: FC<StarRatingProps> = (props) => {
   const stars: ReactNode[] = [];
-  for (var star = 1; star <= Math.round(props.rating); star++) {
-    stars.push(<Star />);
+  for (var currentStar = 1; currentStar <= 5; currentStar++) {
+    if (currentStar <= Math.round(props.rating)) {
+      stars.push(<StarSolid key={currentStar} />);
+    } else {
+      stars.push(<StarRegular key={currentStar} />);
+    }
   }
   return <>{stars}</>;
 };
